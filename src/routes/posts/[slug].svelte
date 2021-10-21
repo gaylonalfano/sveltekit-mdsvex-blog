c48fc440df382cde84f43c782d61dd0e65cdbb6c
gaylonalfano <gaylon.alfano@gmail.com>
2021-10-21 15:49:23 +0800

    Added some redirecting if user goes to a bad path. From the load() function you can return an object with the redirect and status properties. You can do the same for 404 by returning the status and an error property.

--- src/routes/posts/[slug].svelte
+++ src/routes/posts/[slug].svelte
@@ -1,38 +1,51 @@
 <script context="module">
 	// See for Types: https://kit.svelte.dev/docs#loading
 	export async function load({ page }) {
-		// Dynamic import
-		// NOTE Use .default to grab the ProxyComponent. Remove it and you have Module {}
-		// that holds both default AND metadata objects (where Frontmatter is stored)
-		const Hello = await import(`../../posts/${page.params.slug}.md`);
-		console.log('Hello:', Hello); // Module {default, metadata}
+		try {
+			// Dynamic import
+			// NOTE Use .default to grab the ProxyComponent. Remove it and you have Module {}
+			// that holds both default AND metadata objects (where Frontmatter is stored)
+			const Post = await import(`../../posts/${page.params.slug}.md`);
+			console.log('Post:', Post); // Module {default, metadata}
 
-		// const post = {
-		// 	title: page.params.slug,
-		// 	date: new Date(),
-		// 	body: 'lorem ipsum'
-		// };
+			// const post = {
+			// 	title: page.params.slug,
+			// 	date: new Date(),
+			// 	body: 'lorem ipsum'
+			// };
 
-		return {
-			props: {
-				Hello: Hello.default,
-				title: Hello.metadata.title, // From MD Frontmatter!
-				date: Hello.metadata.date
-			}
-		};
+			return {
+				props: {
+					Post: Post.default,
+					title: Post.metadata.title, // From MD Frontmatter!
+					date: Post.metadata.date
+				}
+			};
+		} catch (e) {
+			console.log(e);
+			// NOTE Can redirect to a valid URL using redirect and status 303
+			// return {
+			// 	status: 303,
+			// 	redirect: '/posts'
+			// };
+			// NOTE Or we can return a 404
+			return {
+				status: 404,
+				error: 'Post not found'
+			};
+		}
 	}
 </script>
 
 <script>
+	// NOTE Can import here or inside the load() function
 	/* import Hello from '../../posts/hello.md'; */
 
-	export let Hello;
-	export let title; // You can extract other data from MD files Frontmatter
-	export let date;
+	export let Post;
+	// export let title; // You can extract other data from MD files Frontmatter
+	// export let date;
 	/* export let post; */
 </script>
 
-<h2>{title}</h2>
-<p>I can pull in the 'date' metadata as well: {date}</p>
-
-<Hello />
+<!-- <Post /> is not reactive. Need the svelte:component below instead -->
+<svelte:component this={Post} />
